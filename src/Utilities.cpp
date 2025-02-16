3f6ef619f7d5bfc4b3e8f7cfb793cf7a5f442c13
Jm Casler <jm@casler.org>
2025-02-16 10:33:14 -0800

    Update novalogo to take less space in flash.

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -20,42 +20,60 @@ void safeSerialPrintf(const char* format, ...)
     xSemaphoreGive(serialMutex);
 }
 
+// Helper function: printIndent
+// Prints 'indent' number of spaces to Serial. This creates a dynamic indent to avoid 
+// storing leading space characters in flash memory.
+void printIndent(int indent) {
+    for (int i = 0; i < indent; i++) {
+        Serial.print(' ');
+    }
+}
+
+// Helper function: printFWithIndent
+// First prints an indent using printIndent, then prints a flash-stored string using Serial.println.
+// This helps keep the output visually similar while reducing flash memory usage by not storing
+// leading whitespace in the string literals.
+void printFWithIndent(int indent, const __FlashStringHelper* str) {
+    printIndent(indent);
+    Serial.println(str);
+}
+
 void NovaLogo() {
-    Serial.println("                                                                           ");
-    Serial.println("                                ........                                   ");
-    Serial.println("                            ..::--==+++++==--:..                           ");
-    Serial.println("                           :-=++++==-:::::::---.                           ");
-    Serial.println("                         :=++=-:.:-=+**#####***+-:                         ");
-    Serial.println("                      .:++-::-+#%%%#*+==------=+***-.                      ");
-    Serial.println("                     .-+::=*%%#+-:.   ...:::::....:=-.                     ");
-    Serial.println("                    .:::+%%+-.   :-=*##%%%%@%%%%#*=:..                     ");
-    Serial.println("                     .+%#-.  :=*%%%%%#+==------=+*%%#-                     ");
-    Serial.println("                    .*%-  :=#@@#*=:.     ......    :+%-                    ");
-    Serial.println("                    =#. :+%@#=:.   .-=+*########*+-. :=                    ");
-    Serial.println("                    =. =@@*:.  .-+#@@%%##*++++**#%@%=...                   ");
-    Serial.println("                    ..+@#:  .-*%@%*+-:.          .-*@+                     ");
-    Serial.println("                     :@#.  -#@@*-.   .:=++****+=:. .+#.                    ");
-    Serial.println("                     :%: .+@@+.  .:+#%%%%##**#%%@%- ::                     ");
-    Serial.println("                     .-:.+@%:  .=#@%*=-.      .:+@%..                      ");
-    Serial.println("                      ...*@-  :#@%=:  .-+**#*=. .#*.                       ");
-    Serial.println("                         :#-. *@#.  :*%%*+=+@@=.-=.                        ");
-    Serial.println("                          .-..+@*.  #@%:..:=#+...                          ");
-    Serial.println("                            . .-+=:.:+*+==--. .                            ");
-    Serial.println("                                 .... ....                                 ");
-    Serial.println("                                                                           ");
-    Serial.println("                                                                           ");
-    Serial.println("  ...       ...      ..::---:::.        .-.         ::         ..          ");
-    Serial.println("  .+=       :+.    .-===-----====:      .**        :#-         =#.         ");
-    Serial.println("  .*%=.     -*.  .=*=:..      ..-++.     :%-       +*.        .##+         ");
-    Serial.println("  .+=+:     -*.  -+.             .=*:     **.     :%:        .*+.%-        ");
-    Serial.println("  .*=..     -*.  ..                -#.    :%-     *+         -#. =#.       ");
-    Serial.println("  .*=       -*.                     *=    .*#.   -#.        .#=  .#+       ");
-    Serial.println("  .*=       -*.                     =+     :%-  .#=         =*.   :%:      ");
-    Serial.println("  .*=       -*.                     *=      *#. =#.        :%:    .+*.     ");
-    Serial.println("  .*=     ..-*.                    -#.      :#:.#-        .*+ .:.  .%=     ");
-    Serial.println("  .*=     :+=*.                  .=*:       .:.=*.        =#. .#-   -%:    ");
-    Serial.println("  .*=     .-%*.      ...     ...-+=.          :#:        .#-   =*.  .**.   ");
-    Serial.println("  .+-       =*.      :+=-----==+-.            =+        .+*.   .#-   :#-   ");
-    Serial.println("   ..       ...       .::----:.               ..        .:.     :.    ::   ");
-    Serial.println("                                                                           ");
+    printFWithIndent(0, F(""));
+    printFWithIndent(32, F("........"));
+    printFWithIndent(28, F("..::--==+++++==--:.."));
+    printFWithIndent(27, F(":-=++++==-:::::::---."));
+    printFWithIndent(25, F(":=++=-:.:-=+**#####***+-:"));
+    printFWithIndent(22, F(".:++-::-+#%%%#*+==------=+***-."));
+    printFWithIndent(21, F(".-+::=*%%#+-:.   ...:::::....:=-."));
+    printFWithIndent(20, F(":::+%%+-.   :-=*##%%%%@%%%%#*=:.."));
+    printFWithIndent(21, F(".+%#-.  :=*%%%%%#+==------=+*%%#-"));
+    printFWithIndent(20, F(".*%-  :=#@@#*=:.     ......    :+%-"));
+    printFWithIndent(20, F("=#. :+%@#=:.   .-=+*########*+-. :="));
+    printFWithIndent(20, F("=. =@@*:.  .-+#@@%%##*++++**#%@%=..."));
+    printFWithIndent(20, F("..+@#:  .-*%@%*+-:.          .-*@+"));
+    printFWithIndent(21, F(":@#.  -#@@*-.   .:=++****+=:. .+#."));
+    printFWithIndent(21, F(":%: .+@@+.  .:+#%%%%##**#%%@%- ::"));
+    printFWithIndent(21, F(".-:.+@%:  .=#@%*=-.      .:+@%."));
+    printFWithIndent(22, F("...*@-  :#@%=:  .-+**#*=. .#*."));
+    printFWithIndent(25, F(":#-. *@#.  :*%%*+=+@@=.-=."));
+    printFWithIndent(26, F(".-..+@*.  #@%:..:=#+..."));
+    printFWithIndent(28, F(". .-+=:.:+*+==--. ."));
+    printFWithIndent(33, F(".... ...."));
+    printFWithIndent(0, F(""));
+    printFWithIndent(0, F(""));
+    printFWithIndent(2, F("...       ...      ..::---:::.        .-.         ::         .."));
+    printFWithIndent(2, F(".+=       :+.    .-===-----====:      .**        :#-         =#."));
+    printFWithIndent(2, F(".*%=.     -*.  .=*=:..      ..-++.     :%-       +*.        .##+"));
+    printFWithIndent(2, F(".+=+:     -*.  -+.             .=*:     **.     :%:        .*+.%-"));
+    printFWithIndent(2, F(".*=..     -*.  ..                -#.    :%-     *+         -#. =#."));
+    printFWithIndent(2, F(".*=       -*.                     *=    .*#.   -#.        .#=  .#+"));
+    printFWithIndent(2, F(".*=       -*.                     =+     :%-  .#=         =*.   :%:"));
+    printFWithIndent(2, F(".*=       -*.                     *=      *#. =#.        :%:    .+*."));
+    printFWithIndent(2, F(".*=     ..-*.                    -#.      :#:.#-        .*+ .:.  .%="));
+    printFWithIndent(2, F(".*=     :+=*.                  .=*:       .:.=*.        =#. .#-   -%:"));
+    printFWithIndent(2, F(".*=     .-%*.      ...     ...-+=.          :#:        .#-   =*.  .**."));
+    printFWithIndent(2, F(".+-       =*.      :+=-----==+-.            =+        .+*.   .#-   :#-"));
+    printFWithIndent(2, F("..       ...       .::----:.               ..        .:.     :."));
+
 }
